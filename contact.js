29dc6503d4145562ceb39157aff23735789af3fe
sumejaluma <sumejaluma12@gmail.com>
2023-05-03 00:51:29 +0200

    changes

--- contact.js
+++ contact.js
@@ -3,7 +3,7 @@ const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 const phoneInput = document.getElementById('phone');
-const selectInput = document.getElementById('select');
+
 
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the default form submission
@@ -20,11 +20,7 @@ form.addEventListener('submit', function(event) {
     alert('Please enter your email.');
     emailInput.focus();
     return false;
-  } else if (!isValidEmail(emailInput.value.trim())) {
-    alert('Please enter a valid email.');
-    emailInput.focus();
-    return false;
-  }
+  } 
 
   if (phoneInput.value.trim() === '') {
     alert('Please enter your Phone Number.');
@@ -32,11 +28,6 @@ form.addEventListener('submit', function(event) {
     return false;
   }
 
-  if (selectInput.value.trim() === 'Please Select') {
-    alert('Please select the message subject.');
-    selectInput.focus();
-    return false;
-  }
 
   // Validate the message field
   if (messageInput.value.trim() === '') {
@@ -49,10 +40,6 @@ form.addEventListener('submit', function(event) {
   form.submit();
 });
 
-function isValidEmail(email) {
-  // A basic email validation regular expression
-  const emailRegex = /^\S+@\S+\.\S+$/;
-  return emailRegex.test(email);
-}
+
 
 
